43fccb81ab46ff1ee05042d2c6457745c65345a0
callme-sam <samuele.colmi@chips.it>
2025-08-06 10:01:00 +0200

    blas: Implement all blas-related functions

--- src/blas.c
+++ src/blas.c
@@ -1,21 +1,97 @@
 #include <gsl/gsl_blas.h>
 
+#include "log.h"
+
 #include "blas.h"
 
 int blas_dgemv(blas_trans_e trans, double alpha, const matrix_t *A, const vector_t *x, double beta, vector_t *y)
 {
-    if (trans == BLAS_TRANSPOSE)
-        return gsl_blas_dgemv(CblasTrans, alpha, A->gsl, x->gsl, beta, y->gsl);
-    else
-        return gsl_blas_dgemv(CblasNoTrans, alpha, A->gsl, x->gsl, beta, y->gsl);
+    size_t rows;
+    size_t cols;
+
+    rows = A->size1;
+    cols = A->size2;
+
+    /* Optimization */
+    if (alpha == 0.0) {
+        if (beta == 0.0) {
+            vector_set_zero(y);
+        } else if (beta != 1.0) {
+            vector_scale(y, beta);
+        }
+
+        return 0;
+    }
+
+    if (trans == BLAS_NO_TRANSPOSE) {
+        if (y->size != rows || x->size != cols) {
+            LOG_ERROR("Error: dimension mismatch");
+            return -1;
+        }
+
+        /* y = beta * y + alpha * A * x */
+        for (size_t i = 0; i < rows; i++) {
+            double sum = 0;
+            for (size_t j = 0; j < cols; j++) {
+                sum += A->data[i * cols + j] * x->data[j];
+            }
+            y->data[i] = beta * y->data[i] + alpha * sum;
+        }
+    } else {
+        if (y->size != cols || x->size != rows) {
+            LOG_ERROR("Error: dimension mismatch");
+            return -1;
+        }
+
+        /* y = beta * y + alpha * A^T * x */
+        for (size_t i = 0; i < cols; i++) {
+            double sum = 0;
+            for (size_t j = 0; j < rows; j++) {
+                sum += A->data[j * cols +i] * x->data[j];
+            }
+            y->data[i] = alpha * sum + beta * y->data[i];
+        }
+    }
+
+    return 0;
 }
 
 int blas_ddot(const vector_t *x, const vector_t *y, double *result)
 {
-    return gsl_blas_ddot(x->gsl, y->gsl, result);
+    size_t x_len;
+    size_t y_len;
+
+    x_len = x->size;
+    y_len = y->size;
+    if (x_len != y_len) {
+        LOG_ERROR("Error: Vectors must have the same length");
+        return -1;
+    }
+
+    *result = 0;
+    for (size_t i = 0; i < x_len; i++)
+        *result += x->data[i] * y->data[i];
+
+    return 0;
 }
 
 int blas_daxpy(double alpha, const vector_t *x, vector_t *y)
 {
-    return gsl_blas_daxpy(alpha, x->gsl, y->gsl);
+    size_t x_len;
+    size_t y_len;
+
+    x_len = x->size;
+    y_len = y->size;
+    if (x_len != y_len) {
+        LOG_ERROR("Error: Vectors must have the same length");
+        return -1;
+    }
+
+    if (alpha == 0.0)
+        return 0;
+
+    for (size_t i = 0; i < x_len; i++)
+        y->data[i] += alpha * x->data[i];
+
+    return 0;
 }
